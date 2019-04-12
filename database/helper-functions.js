d94da09478e3c1eeca45289bb47c8d338b46d61c
novinary <novina.dev@gmail.com>
2019-04-12 09:28:12 +0100

    Create middleware for authentication using json web tokens + work on login function

--- database/helper-functions.js
+++ database/helper-functions.js
@@ -2,7 +2,8 @@ const db = require('./dbConfig');
 
 module.exports = {
     add,
-    find
+    find,
+    findBy
 };
 
 function find() {
@@ -13,4 +14,8 @@ async function add(user) {
     const [id] = await db('users').insert(user);
 
     return findById(id);
+}
+
+function findBy(filter) {
+    return db('users').where(filter);
 }
